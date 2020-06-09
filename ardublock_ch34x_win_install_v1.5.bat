9010215ebaf645b29631fef0be6bf50b6f573254
Edward Li <edward@barnabasrobotics.com>
2020-06-08 17:14:34 -0700

    updated routine to correctly add files to all profiles

---
+++ ardublock_ch34x_win_install_v1.5.bat
@@ -0,0 +1,41 @@
+@echo off
+set LOGFILE=batch.log
+call :LOG > %LOGFILE%
+exit /B
+
+:LOG
+
+title Barnabas Ardublock Install For Windows
+echo Version 1.5
+
+for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
+set mytime=%time%
+echo Current time is %mydate%:%mytime%
+
+: Install CH341 Driver
+echo Installing CH341 Driver...
+cd CH341SER
+start /w setup.exe /s
+cd..
+
+: Create libraries and tools folders
+set LIBPATH=Documents\Arduino\libraries
+set ARDUPATH=Documents\Arduino\tools\ArdublockTool\tool
+
+: Copy Ardublock file and libraries for all windows users (including subdirectories)
+for /d %%a in (C:\Users\*) do (
+
+    @echo:
+    echo Copying files for %%a
+    @echo: 
+
+    IF NOT EXIST "%%a\%LIBPATH%" (
+        md "%%a\%LIBPATH%"
+    )
+    IF NOT EXIST "%%a\%ARDUPATH%" (
+        md "%%a\%ARDUPATH%"
+    )
+    xcopy /y %CD%\libraries\* "%%a\%LIBPATH%" /s /i
+    xcopy /y %CD%\tools\ArduBlockTool\tool\* "%%a\%ARDUPATH%" /s /i
+)
+
